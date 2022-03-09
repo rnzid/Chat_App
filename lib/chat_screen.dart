cc8624268df0a6b8e3b4d0af92a293baea4e21f3
rnzid <ranjitdangol13@gmail.com>
2022-03-09 21:14:37 +0545

    Messege Box Implemented

--- lib/chat_screen.dart
+++ lib/chat_screen.dart
@@ -8,8 +8,34 @@ class ChatScreen extends StatefulWidget {
 }
 
 class _ChatScreenState extends State<ChatScreen> {
+  final TextEditingController _textController = TextEditingController();
+  void _handleSubmitted(String text) {
+    _textController.clear();
+  }
+
+  Widget _textComposerWidget() {
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: 8.0),
+      child: Row(
+        children: <Widget>[
+          Flexible(
+            child: TextField(
+              decoration: InputDecoration.collapsed(hintText: "Send Messege"),
+              controller: _textController,
+              onSubmitted: _handleSubmitted,
+            ),
+          ),
+          Container(
+            margin: const EdgeInsets.symmetric(horizontal: 4.0),
+            child: IconButton(onPressed: () {}, icon: Icon(Icons.send)),
+          )
+        ],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return _textComposerWidget();
   }
 }
