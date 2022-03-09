05a72e4dc69bee3f46524222a520c812ecf87eb9
rnzid <ranjitdangol13@gmail.com>
2022-03-09 22:41:23 +0545

    ChatBox with messege complete

--- lib/chat_screen.dart
+++ lib/chat_screen.dart
@@ -1,3 +1,4 @@
+import 'package:chatbox/chat_messege.dart';
 import 'package:flutter/material.dart';
 
 class ChatScreen extends StatefulWidget {
@@ -9,33 +10,63 @@ class ChatScreen extends StatefulWidget {
 
 class _ChatScreenState extends State<ChatScreen> {
   final TextEditingController _textController = TextEditingController();
+  final List<ChatMessege> _message = <ChatMessege>[];
   void _handleSubmitted(String text) {
     _textController.clear();
+    ChatMessege messege = ChatMessege(text: text);
+    setState(() {
+      _message.insert(0, messege);
+    });
   }
 
   Widget _textComposerWidget() {
-    return Container(
-      margin: EdgeInsets.symmetric(horizontal: 8.0),
-      child: Row(
-        children: <Widget>[
-          Flexible(
-            child: TextField(
-              decoration: InputDecoration.collapsed(hintText: "Send Messege"),
-              controller: _textController,
-              onSubmitted: _handleSubmitted,
+    return IconTheme(
+      data: IconThemeData(color: Colors.blue),
+      child: Container(
+        margin: EdgeInsets.symmetric(horizontal: 8.0),
+        child: Row(
+          children: <Widget>[
+            Flexible(
+              child: TextField(
+                decoration: InputDecoration.collapsed(hintText: "Send Messege"),
+                controller: _textController,
+                onSubmitted: _handleSubmitted,
+              ),
             ),
-          ),
-          Container(
-            margin: const EdgeInsets.symmetric(horizontal: 4.0),
-            child: IconButton(onPressed: () {}, icon: Icon(Icons.send)),
-          )
-        ],
+            Container(
+              margin: const EdgeInsets.symmetric(horizontal: 4.0),
+              child: IconButton(
+                  onPressed: () => _handleSubmitted(_textController.text),
+                  icon: Icon(Icons.send)),
+            )
+          ],
+        ),
       ),
     );
   }
 
   @override
   Widget build(BuildContext context) {
-    return _textComposerWidget();
+    return Column(
+      children: <Widget>[
+        Flexible(
+          child: ListView.builder(
+            itemBuilder: (_, int index) => _message[index],
+            padding: const EdgeInsets.all(8.0),
+            itemCount: _message.length,
+            reverse: true,
+          ),
+        ),
+        const Divider(
+          height: 1.0,
+        ),
+        Container(
+          decoration: BoxDecoration(
+            color: Theme.of(context).cardColor,
+          ),
+          child: _textComposerWidget(),
+        )
+      ],
+    );
   }
 }
